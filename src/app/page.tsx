a887b962678dd9d54f023e877655af4efb9d94ec
Dmytro <104273286+dmtsolovey@users.noreply.github.com>
2024-10-12 13:45:41 +0300

    add yml

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    }}>
+      <h1>
+        Welcome to NPM_PACKAGE TEST ENVIRONMENT
+      </h1>
+    </div>
+  );
+}
